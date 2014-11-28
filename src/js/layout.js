f040e34f60e3f31a7aff3805d78138d6f7887103
Nico <nic.girault@gmail.com>
2014-11-28 16:01:21 +0100

    remove radial labels

--- src/js/layout.js
+++ src/js/layout.js
@@ -1,7 +1,6 @@
 function layout() {
     var innerRadius = 50,
     outerRadius = 80,
-    segmentHeight = 20,
     domain = null,
     accessor = function(d) {return d;},
     radialLabels = segmentLabels = [];
@@ -36,31 +35,6 @@ function layout() {
             // Unique id so that the text path defs are unique - is there a better way to do this?
             var id = d3.selectAll(".circos-layout")[0].length;
 
-            //Radial labels
-            var lsa = 0.01; //Label start angle
-            var labels = svg.append("g")
-                .classed("labels", true)
-                .classed("radial", true)
-                .attr("transform", "translate(" + parseInt(offset) + "," + parseInt(offset) + ")");
-
-            labels.selectAll("def")
-                .data(radialLabels).enter()
-                .append("def")
-                .append("path")
-                .attr("id", function(d, i) {return "radial-label-path-"+id+"-"+i;})
-                .attr("d", function(d, i) {
-                    var r = innerRadius + ((i + 0.2) * segmentHeight);
-                    return "m" + r * Math.sin(lsa) + " -" + r * Math.cos(lsa) + 
-                            " a" + r + " " + r + " 0 1 1 -1 0";
-                });
-
-            labels.selectAll("text")
-                .data(radialLabels).enter()
-                .append("text")
-                .append("textPath")
-                .attr("xlink:href", function(d, i) {return "#radial-label-path-"+id+"-"+i;})
-                .style("font-size", 0.6 * segmentHeight + 'px')
-                .text(function(d) {return d;});
         });
 
     }
@@ -86,12 +60,6 @@ function layout() {
     chart.outerRadius = function(_) {
         if (!arguments.length) return outerRadius;
         outerRadius = _;
-        return chart;
-    };
-
-    chart.segmentHeight = function(_) {
-        if (!arguments.length) return segmentHeight;
-        segmentHeight = _;
         return chart;
     };
 
