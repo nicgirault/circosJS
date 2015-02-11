44afc08ac25de26ccf0d9a63d8d862975b33c109
Nico <nic.girault@gmail.com>
2015-02-11 20:47:07 +0100

    change demo

--- demo/demo.js
+++ demo/demo.js
@@ -458,15 +458,10 @@ var heatmap = [
 ]
 
 links = [
-    {source: {id: 'chr1', start: 1, end: 100000000}, target: {id: 'chr15', start: 1, end: 10000000}, value: 0},
-    {source: {id: 'chr2', start: 150000000, end: 200000000}, target: {id: 'chr8', start: 1, end: 40000000}, value: 1},
-    {source: {id: 'chr4', start: 20000000, end: 40000000}, target: {id: 'chr3', start: 1, end: 40000000}, value: 2},
-    {source: {id: 'chr5', start: 20000000, end: 40000000}, target: {id: 'chr21', start: 1, end: 40000000}, value: 3},
-    {source: {id: 'chr6', start: 20000000, end: 40000000}, target: {id: 'chr8', start: 1, end: 40000000}, value: 4},
-    {source: {id: 'chr7', start: 20000000, end: 40000000}, target: {id: 'chr12', start: 1, end: 40000000}, value: 5},
-    {source: {id: 'chr8', start: 20000000, end: 40000000}, target: {id: 'chrX', start: 1, end: 40000000}, value: 5},
-    {source: {id: 'chr9', start: 20000000, end: 40000000}, target: {id: 'chr2', start: 1, end: 40000000}, value: 6},
-    {source: {id: 'chr10', start: 20000000, end: 40000000}, target: {id: 'chr7', start: 1, end: 40000000}, value: 7}
+    {source: {id: 'january', start: 1, end: 10}, target: {id: 'february', start: 20, end: 25}, value: 0},
+    {source: {id: 'march', start: 1, end: 10}, target: {id: 'december', start: 20, end: 25}, value: 1},
+    {source: {id: 'march', start: 8, end: 10}, target: {id: 'august', start: 7, end: 25}, value: 2},
+    {source: {id: 'april', start: 13, end: 15}, target: {id: 'november', start: 10, end: 25}, value: 3}
 ];
 
 scatter = [
@@ -547,11 +542,11 @@ circos
         },
         layout_data
     )
-    // .heatmap('h1', {innerRadius: 220, outerRadius: 240, logScale: true}, heatmap)
-    // .histogram('hist1', {innerRadius: 200, outerRadius: 220}, heatmap)
+    .heatmap('h1', {innerRadius: 220, outerRadius: 240, logScale: true}, heatmap)
+    .histogram('hist1', {innerRadius: 200, outerRadius: 220}, heatmap)
     .scatter('c1', {innerRadius: 150, outerRadius: 200, glyph: {shape: 'cross', size: 50, fill: true}}, scatter, rules, backgrounds)
     .line('line1', {innerRadius: 150, outerRadius: 200, interpolation: 'cardinal'}, scatter)
-    // .chord('l1', {}, links)
+    .chord('l1', {}, links)
     .stack('stack1', {thickness: 10, usePalette: true, margin: 0, direction: 'out', innerRadius: 190, outerRadius: 240}, stack, stack_rules)
     .line('line2', {innerRadius:100, outerRadius: 200}, [], {}, backgrounds2)
     .render();
