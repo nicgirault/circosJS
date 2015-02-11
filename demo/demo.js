9bab2b0fb740371ab31a0a38a7f8a37d55fdcce0
Nico <nic.girault@gmail.com>
2015-02-11 20:47:07 +0100

    change demo

--- demo/demo.js
+++ demo/demo.js
@@ -547,11 +547,11 @@ circos
         },
         layout_data
     )
-    // .heatmap('h1', {innerRadius: 220, outerRadius: 240, logScale: true}, heatmap)
-    // .histogram('hist1', {innerRadius: 200, outerRadius: 220}, heatmap)
+    .heatmap('h1', {innerRadius: 220, outerRadius: 240, logScale: true}, heatmap)
+    .histogram('hist1', {innerRadius: 200, outerRadius: 220}, heatmap)
     .scatter('c1', {innerRadius: 150, outerRadius: 200, glyph: {shape: 'cross', size: 50, fill: true}}, scatter, rules, backgrounds)
     .line('line1', {innerRadius: 150, outerRadius: 200, interpolation: 'cardinal'}, scatter)
-    // .chord('l1', {}, links)
+    .chord('l1', {}, links)
     .stack('stack1', {thickness: 10, usePalette: true, margin: 0, direction: 'out', innerRadius: 190, outerRadius: 240}, stack, stack_rules)
     .line('line2', {innerRadius:100, outerRadius: 200}, [], {}, backgrounds2)
     .render();
