941eaf128f696383088b6d6f2096d4ef8b1cae3f
Braxton Robbason <braxton.robbason@gmail.com>
2020-04-27 15:20:33 -0400

    AFF-121: Highlight blocks too

--- src/behaviors/highlight.js
+++ src/behaviors/highlight.js
@@ -4,19 +4,28 @@ import 'd3-transition'
 export function registerHighlight (track, instance, element, trackParams) {
   track.dispatch.on('mouseover.highlight', (d) => {
     instance.svg.selectAll("path.chord").attr("opacity", 0.1)
+    instance.svg.selectAll(
+      "g.cs-layout").selectAll("g").attr("opacity", 0.1)
+
     if (d.source){
       // User has moused over a chord
       instance.svg.selectAll(
       "path.chord[data-source='"+d.source.id+"'][data-target='"+d.target.id+"']"
       ).attr("opacity", 0.7)
-      instance.svg.selectAll("path.chord").sort(
+
+      instance.svg.selectAll(
+        "g.cs-layout").selectAll("g."+d.source.id).attr("opacity", 1)
+      instance.svg.selectAll(
+        "g.cs-layout").selectAll("g."+d.target.id).attr("opacity", 1)
+
+      /*instance.svg.selectAll("path.chord").sort(
         // Order this chord last so it appears on top
         function (a, b) {
           if (a.source.id == d.source.id && a.target.id == d.target.id){
             return 1
           }
           else return -1
-        })
+        })*/
     }
     if(d.block_id){
       // For layout arcs, highlight all incoming or outgoing chords
@@ -24,18 +33,23 @@ export function registerHighlight (track, instance, element, trackParams) {
         "path.chord[data-source='"+d.block_id+"']").attr("opacity", 0.7)
       instance.svg.selectAll(
         "path.chord[data-target='"+d.block_id+"']").attr("opacity", 0.7)
-      instance.svg.selectAll("path.chord").sort(
+      // blocks
+      instance.svg.selectAll(
+        "g.cs-layout").selectAll("g."+d.block_id).attr("opacity", 1)
+      /*instance.svg.selectAll("path.chord").sort(
         // Put these chords on top
         function (a, b) {
           if (a.source.id == d.block_id || a.target.id == d.block_id){
             return 1
           }
           else return -1
-        })
+        })*/
     }
   })
 
   track.dispatch.on('mouseout.highlight', (d) => {
     instance.svg.selectAll("path.chord").attr("opacity", 0.7)
+    instance.svg.selectAll(
+        "g.cs-layout").selectAll("g").attr("opacity", 1)
   })
 }
